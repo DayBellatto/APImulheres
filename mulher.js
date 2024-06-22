e7601a8e818d08a1ec6223773cc68bf70244b0df
DayBellatto <motasantos.daiana@yahoo.com.br>
2024-06-22 10:13:29 -0300

    salvando alterações

--- mulher.js
+++ mulher.js
@@ -1,19 +1,40 @@
 const express = require("express")
+
 const router = express.Router()
 
+
+
 const app = express()
-const porta = 3333;
 
-function mostraMulher(request,response){
-    response.json ({
-        nome: 'Daiana Bellatto',
-        Bio: 'Desenvolvedora full stack'
-    })
-    }
+const porta = 3333
 
-function mostraPorta() {
-    console.log('Servidor criado e rodando na porta ', porta)
+
+
+function mostraMulher(request, response) {
+
+ response.json({
+
+    nome: 'Simara Conceição',
+
+    imagem: 'https://github.com/simaraconceicao.png',
+
+    minibio: 'Desenvolvedora e instrutora'
+
+ })
+
 }
 
-app.use(router.get('/mulher',mostraMulher))
-app.listen(porta,mostraPorta)
+
+
+function mostraPorta() {
+
+    console.log("Servidor criado e rodando na porta ", porta)
+
+}
+
+
+
+app.use(router.get('/mulher', mostraMulher))
+
+app.listen(porta, mostraPorta)
+
